6d39b46f35463046069609ce5c0e874547635a4c
Vinit Bolla <vinitbolla10@gmail.com>
2020-04-17 14:09:03 +0530

    change mongo uri

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   port: process.env.PORT || 5000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri:
-    "mongodb+srv://dbVinit:vinit@cluster0-czs7p.mongodb.net/test?retryWrites=true&w=majority",
+    'mongodb+srv://dbVinit:vinit@cluster0-czs7p.mongodb.net/test?retryWrites=true&w=majority',
 };
 
 module.exports = config;
